8258364aae239c86d9c2fd1b7f5166ae03ef6625
paola1492 <68004535+paola1492@users.noreply.github.com>
2022-05-13 16:15:13 +0200

    Font adjust to min temperature

--- src/script.js
+++ src/script.js
@@ -53,7 +53,7 @@ function displayForecast(response) {
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
             forecastDay.temp.max
-          )}° </span>
+          )}° /  </span>
           <span class="weather-forecast-temperature-min"> ${Math.round(
             forecastDay.temp.min
           )}° </span>
